49630a1af641c6b558e6e0dc4d20264a695f93d0
nahknarmi <ikhan@thoughtworks.com>
2020-02-07 12:51:22 -0500

    Refactoring

--- src/main/java/net/nahknarmi/arch/adapter/out/ArchitectureDataStructureWriter.java
+++ src/main/java/net/nahknarmi/arch/adapter/out/ArchitectureDataStructureWriter.java
@@ -19,8 +19,8 @@ import org.apache.commons.logging.LogFactory;
 import java.io.File;
 import java.io.IOException;
 
-public class WorkspaceWriter {
-    private static final Log logger = LogFactory.getLog(WorkspaceWriter.class);
+public class ArchitectureDataStructureWriter {
+    private static final Log logger = LogFactory.getLog(ArchitectureDataStructureWriter.class);
 
     public File export(ArchitectureDataStructure dataStructure) throws IOException {
         File tempFile = File.createTempFile("arch-as-code", ".yml");
