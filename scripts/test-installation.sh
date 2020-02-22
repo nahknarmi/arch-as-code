5c1714086359c021b80ab9dd56f0bdbd71de1ebb
nahknarmi <ikhan@thoughtworks.com>
2020-02-22 14:02:04 -0500

    WIP

--- scripts/test-installation.sh
+++ scripts/test-installation.sh
@@ -1,29 +1,11 @@
 #!/usr/bin/env bash
 set -eux
 
-
 mkdir -p ~/arch-as-code && curl -s https://api.github.com/repos/trilogy-group/arch-as-code/releases/latest | grep "browser_download_url" | cut -d : -f 2,3 | tr -d \" | xargs curl -L | tar --strip-components 1 -x -C ~/arch-as-code
-#mkdir -p ~/arch-as-code
-#
-#curl -i https://api.github.com/repos/trilogy-group/arch-as-code/releases/latest
-#
-#curl -v https://api.github.com/repos/trilogy-group/arch-as-code/releases/latest
-#
-#echo `curl -s https://api.github.com/repos/trilogy-group/arch-as-code/releases/latest | grep "browser_download_url"`
-#
-#
-#echo `curl -s https://api.github.com/repos/trilogy-group/arch-as-code/releases/latest | grep "browser_download_url" | cut -d : -f 2,3 | tr -d \"`
-#
-#curl -s https://api.github.com/repos/trilogy-group/arch-as-code/releases/latest | grep "browser_download_url" | cut -d : -f 2,3 | tr -d \" | xargs curl -L | tar --strip-components 1 -x -C ~/arch-as-code
 
 export PATH=$PATH:~/arch-as-code/bin
 
 arch-as-code --help
-
-
-#export PATH=$PATH:~/arch-as-code/bin
-#
-#~/arch-as-code/bin/arch-as-code --help
 
 mkdir -p ~/my-awesome-product
 cd ~/my-awesome-product
