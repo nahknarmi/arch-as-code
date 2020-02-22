47ff64fb3d5babb58322ed4274ad1f7561167b02
nahknarmi <ikhan@thoughtworks.com>
2020-02-22 14:53:42 -0500

    WIP

---
+++ scripts/install/windows-latest/install.ps1
@@ -0,0 +1,9 @@
+$download_url = (Invoke-WebRequest "https://api.github.com/repos/trilogy-group/arch-as-code/releases/latest" | ConvertFrom-Json).assets.browser_download_url
+
+(New-Object System.Net.WebClient).DownloadFile($download_url, "$env:temp\arch-as-code.tar.gz")
+
+New-Item -ItemType Directory -Force -Path $HOME\arch-as-code
+
+tar -xzv --strip-components 1 -f $env:temp\arch-as-code.tar.gz -C $HOME\arch-as-code\
+
+$Env:Path += ";$HOME\arch-as-code\bin"
