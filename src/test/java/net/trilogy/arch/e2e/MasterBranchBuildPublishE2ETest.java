542868e15ccbe512920af3eaaf0b771558e349dd
Nikola Savic <nsavic@thoughtworks.com>
2020-08-12 10:37:07 -0400

    Test that documentation is valid without publishing to Structurizr

--- src/test/java/net/trilogy/arch/e2e/MasterBranchBuildPublishE2ETest.java
+++ src/test/java/net/trilogy/arch/e2e/MasterBranchBuildPublishE2ETest.java
@@ -10,12 +10,12 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class MasterBranchBuildPublishE2ETest {
 
-  @Test
-  public void publish() throws Exception {
-    File documentationRoot = new File("documentation/products/arch-as-code");
+    @Test
+    public void validate() {
+        File documentationRoot = new File("documentation/products/arch-as-code");
 
-    Integer statusCode = execute("publish", documentationRoot.getAbsolutePath());
+        Integer statusCode = execute("validate", documentationRoot.getAbsolutePath());
 
-    assertThat(statusCode, equalTo(0));
-  }
+        assertThat(statusCode, equalTo(0));
+    }
 }
