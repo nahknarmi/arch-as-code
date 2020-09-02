94a0431b2df6c5af61fe6dfd3419e23d358fdebf
Brian Oxley <boxley@thoughtworks.com>
2020-09-02 14:50:21 -0500

    Cleaner

--- src/test/java/net/trilogy/arch/adapter/jira/RetrofitOrBust.java
+++ src/test/java/net/trilogy/arch/adapter/jira/RetrofitOrBust.java
@@ -1,6 +1,6 @@
 package net.trilogy.arch.adapter.jira;
 
-import retrofit2.Retrofit;
+import retrofit2.Retrofit.Builder;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
 import java.io.IOException;
@@ -8,17 +8,17 @@ import java.io.IOException;
 import static java.lang.System.out;
 
 public class RetrofitOrBust {
-    public static void main(String[] args) throws IOException {
-        // TODO: Hold onto this instance for global reuse
-        final var retrofit = new Retrofit.Builder()
-                .baseUrl("https://tw-trilogy.atlassian.net/")
-                .addConverterFactory(JacksonConverterFactory.create())
-                .build();
+    private static final String trilogyBaseUrl = "https://tw-trilogy.atlassian.net/";
 
-        // TODO: Hold onto this instance for global reuse; see above TODO
-        final var jira = retrofit.create(RemoteJira.class);
+    public static final RemoteJira REMOTE_JIRA = new Builder()
+            .baseUrl(trilogyBaseUrl)
+            .addConverterFactory(JacksonConverterFactory.create())
+            .build()
+            .create(RemoteJira.class);
 
-        final var issue = jira.browseIssue("AAC-129").execute();
+    public static void main(final String... args) throws IOException {
+        final var issue = REMOTE_JIRA.browseIssue("AAC-129").execute();
+
         out.println("issue response = " + issue);
     }
 }
