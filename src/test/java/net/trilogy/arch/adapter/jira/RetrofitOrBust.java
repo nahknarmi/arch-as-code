99fbafc9fe42fc3e4b9ed974e210058c12c24426
Brian Oxley <boxley@thoughtworks.com>
2020-09-02 12:13:31 -0500

    TODO

--- src/test/java/net/trilogy/arch/adapter/jira/RetrofitOrBust.java
+++ src/test/java/net/trilogy/arch/adapter/jira/RetrofitOrBust.java
@@ -9,12 +9,13 @@ import static java.lang.System.out;
 
 public class RetrofitOrBust {
     public static void main(String[] args) throws IOException {
+        // TODO: Hold onto this instance for global reuse
         final var retrofit = new Retrofit.Builder()
                 .baseUrl("https://tw-trilogy.atlassian.net/")
                 .addConverterFactory(JacksonConverterFactory.create())
                 .build();
 
-        // TODO: Hold onto this instance globally for reuse
+        // TODO: Hold onto this instance for global reuse; see above TODO
         final var jira = retrofit.create(RemoteJira.class);
 
         final var issue = jira.browseIssue("AAC-129").execute();
