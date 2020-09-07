4557148d85b4652509dfacffd19d698772695ce7
Brian Oxley <boxley@thoughtworks.com>
2020-09-07 13:45:10 -0500

    Simpler

--- src/main/java/net/trilogy/arch/adapter/jira/JiraCreateStoryStatus.java
+++ src/main/java/net/trilogy/arch/adapter/jira/JiraCreateStoryStatus.java
@@ -2,27 +2,27 @@ package net.trilogy.arch.adapter.jira;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import static lombok.AccessLevel.PRIVATE;
 
 @Getter
 @EqualsAndHashCode
+@RequiredArgsConstructor(access = PRIVATE)
 public class JiraCreateStoryStatus {
-    private final boolean succeeded;
     private final String issueKey;
     private final String issueLink;
     private final String error;
 
-    private JiraCreateStoryStatus(boolean succeeded, String issueKey, String issueLink, String error) {
-        this.succeeded = succeeded;
-        this.issueKey = issueKey;
-        this.issueLink = issueLink;
-        this.error = error;
-    }
-
     public static JiraCreateStoryStatus failed(String error) {
-        return new JiraCreateStoryStatus(false, null, null, error);
+        return new JiraCreateStoryStatus(null, null, error);
     }
 
     public static JiraCreateStoryStatus succeeded(String issueKey, String issueLink) {
-        return new JiraCreateStoryStatus(true, issueKey, issueLink, null);
+        return new JiraCreateStoryStatus(issueKey, issueLink, null);
+    }
+
+    public boolean isSuccess() {
+        return null == error;
     }
 }
