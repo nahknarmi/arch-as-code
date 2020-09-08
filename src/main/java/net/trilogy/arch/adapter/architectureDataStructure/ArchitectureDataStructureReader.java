4467f8880a237f16d57065ec92094a66a4d772f9
Brian Oxley <boxley@thoughtworks.com>
2020-09-08 14:01:03 -0500

    More idiomatic modern Java

--- src/main/java/net/trilogy/arch/adapter/architectureDataStructure/ArchitectureDataStructureReader.java
+++ src/main/java/net/trilogy/arch/adapter/architectureDataStructure/ArchitectureDataStructureReader.java
@@ -7,6 +7,8 @@ import net.trilogy.arch.facade.FilesFacade;
 import java.io.File;
 import java.io.IOException;
 
+import static net.trilogy.arch.adapter.architectureDataStructure.ArchitectureDataStructureObjectMapper.ADS_OBJECT_MAPPER;
+
 /**
  * This class is being strangle-patterned away. Use {@link
  * net.trilogy.arch.adapter.architectureDataStructure.ArchitectureDataStructureObjectMapper
@@ -18,7 +20,7 @@ public class ArchitectureDataStructureReader {
     private final FilesFacade filesFacade;
 
     public ArchitectureDataStructure load(File manifest) throws IOException {
-        final String archAsString = filesFacade.readString(manifest.toPath());
-        return new ArchitectureDataStructureObjectMapper().readValue(archAsString);
+        final String architectureAsString = filesFacade.readString(manifest.toPath());
+        return ADS_OBJECT_MAPPER.readValue(architectureAsString, ArchitectureDataStructure.class);
     }
 }
--- src/main/java/net/trilogy/arch/publish/ArchitectureDataStructurePublisher.java
+++ src/main/java/net/trilogy/arch/publish/ArchitectureDataStructurePublisher.java
@@ -1,7 +1,6 @@
 package net.trilogy.arch.publish;
 
 import com.structurizr.Workspace;
-import net.trilogy.arch.adapter.architectureDataStructure.ArchitectureDataStructureObjectMapper;
 import net.trilogy.arch.adapter.structurizr.StructurizrAdapter;
 import net.trilogy.arch.domain.ArchitectureDataStructure;
 import net.trilogy.arch.facade.FilesFacade;
@@ -10,6 +9,8 @@ import net.trilogy.arch.transformation.TransformerFactory;
 
 import java.io.File;
 import java.io.IOException;
+
+import static net.trilogy.arch.adapter.architectureDataStructure.ArchitectureDataStructureObjectMapper.ADS_OBJECT_MAPPER;
 
 public class ArchitectureDataStructurePublisher {
     private final File productArchitectureDirectory;
@@ -40,7 +41,7 @@ public class ArchitectureDataStructurePublisher {
     }
 
     public void publish() throws IOException {
-        Workspace workspace = getWorkspace(productArchitectureDirectory, manifestFileName);
+        final var workspace = getWorkspace(productArchitectureDirectory, manifestFileName);
 
         if (!structurizrAdapter.publish(workspace)) {
             throw new RuntimeException("Failed to publish to Structurizr");
@@ -48,15 +49,15 @@ public class ArchitectureDataStructurePublisher {
     }
 
     public Workspace getWorkspace(File productArchitectureDirectory, String manifestFileName) throws IOException {
-        ArchitectureDataStructure dataStructure = loadProductArchitecture(productArchitectureDirectory, manifestFileName);
+        final var dataStructure = loadProductArchitecture(productArchitectureDirectory, manifestFileName);
 
         return dataStructureTransformer.toWorkSpace(dataStructure);
     }
 
     public ArchitectureDataStructure loadProductArchitecture(File productArchitectureDirectory, String manifestFileName) throws IOException {
-        File manifestFile = new File(productArchitectureDirectory + File.separator + manifestFileName);
-        String archAsString = this.filesFacade.readString(manifestFile.toPath());
+        final var manifestFile = new File(productArchitectureDirectory + File.separator + manifestFileName);
+        final var architectureAsString = this.filesFacade.readString(manifestFile.toPath());
 
-        return new ArchitectureDataStructureObjectMapper().readValue(archAsString);
+        return ADS_OBJECT_MAPPER.readValue(architectureAsString, ArchitectureDataStructure.class);
     }
 }
