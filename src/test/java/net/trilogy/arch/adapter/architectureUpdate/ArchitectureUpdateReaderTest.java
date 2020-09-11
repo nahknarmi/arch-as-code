1c7de38419c380c43d5789ae025216e2f7647667
Ahmed Elshamy <aselshamy@gmail.com>
2020-09-11 09:26:37 -0500

    Ahmed, ACC-160, added a missing test for assign tdd file contents to tdds.

--- src/test/java/net/trilogy/arch/adapter/architectureUpdate/ArchitectureUpdateReaderTest.java
+++ src/test/java/net/trilogy/arch/adapter/architectureUpdate/ArchitectureUpdateReaderTest.java
@@ -1,6 +1,7 @@
 package net.trilogy.arch.adapter.architectureUpdate;
 
 import net.trilogy.arch.TestHelper;
+import net.trilogy.arch.domain.architectureUpdate.Tdd;
 import net.trilogy.arch.domain.architectureUpdate.Tdd.TddId;
 import net.trilogy.arch.domain.architectureUpdate.TddContent;
 import net.trilogy.arch.facade.FilesFacade;
@@ -11,6 +12,7 @@ import org.junit.rules.ErrorCollector;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Optional;
 
 import static java.util.stream.Collectors.toList;
 import static net.trilogy.arch.TestHelper.ROOT_PATH_TO_TEST_AU_DIRECTORY_STRUCTURE;
@@ -19,6 +21,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public class ArchitectureUpdateReaderTest {
@@ -31,11 +34,6 @@ public class ArchitectureUpdateReaderTest {
     public void setUp() {
         final var rootDir = TestHelper.getPath(getClass(), ROOT_PATH_TO_TEST_AU_DIRECTORY_STRUCTURE);
         auDir = rootDir.resolve("architecture-updates/sample/");
-    }
-
-    @Test
-    public void shouldReadProductArchitectureAndMarkdownFiles() throws Exception {
-
     }
 
     @Test
@@ -70,6 +68,24 @@ public class ArchitectureUpdateReaderTest {
 
         collector.checkThat(architectureUpdate.getName(), equalTo("test"));
         collector.checkThat(first(architectureUpdate.getTddContents()), equalTo(new TddContent("" +
+                "## TDD 1.2\n" +
+                "### Content\n" +
+                "**Lorem ipsum** dolor sit amet:\n" +
+                "* consectetur adipiscing elit\n" +
+                "* sed do eiusmod tempor *incididunt ut labore* et dolore magna aliqua\n" +
+                "* et ligula ullamcorper malesuada proin libero nunc consequat\n",
+                "TDD 1.2 : Component-16.md")));
+    }
+
+    @Test
+    public void tdd_include_file_contents() throws IOException {
+        final var architectureUpdate = new ArchitectureUpdateReader(new FilesFacade()).loadArchitectureUpdate(auDir);
+
+        final Tdd tdd = first(architectureUpdate.getTddContainersByComponent()).getTdds().get(new TddId("TDD 1.2"));
+        assertNotNull(tdd);
+        final Optional<TddContent> expectedContents = tdd.getContent();
+        assertTrue(expectedContents.isPresent());
+        collector.checkThat(expectedContents.get(), equalTo(new TddContent("" +
                 "## TDD 1.2\n" +
                 "### Content\n" +
                 "**Lorem ipsum** dolor sit amet:\n" +
