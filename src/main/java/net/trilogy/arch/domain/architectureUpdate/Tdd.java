ef840e7638a823d630dca9d1a2f74855eb92ee7e
Brian Oxley <boxley@thoughtworks.com>
2020-09-15 16:23:44 -0500

    Good taste

--- src/main/java/net/trilogy/arch/domain/architectureUpdate/Tdd.java
+++ src/main/java/net/trilogy/arch/domain/architectureUpdate/Tdd.java
@@ -10,11 +10,18 @@ import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 import static com.fasterxml.jackson.annotation.JsonCreator.Mode.PROPERTIES;
+import static lombok.AccessLevel.PRIVATE;
 
 @Getter
 @ToString
 @EqualsAndHashCode
+@RequiredArgsConstructor(access = PRIVATE)
 public class Tdd {
+    private static final String SAMPLE_TDD_TEXT = "[SAMPLE TDD TEXT LONG TEXT FORMAT]\nLine 2\nLine 3";
+    private static final String SAMPLE_BLANK_TDD_ID = "[SAMPLE-TDD-ID]";
+    private static final String SAMPLE_NO_PR_TDD_ID = "no-PR";
+    private static final String SAMPLE_BLANK_TDD_COMPONENT_REFERENCE = "[SAMPLE-COMPONENT-ID]";
+
     @JsonProperty(value = "text")
     private final String text;
     // TODO: Why isn't this a JDK Path object?
@@ -31,16 +38,8 @@ public class Tdd {
         this(text, file, null);
     }
 
-    private Tdd(String text,
-                String file,
-                TddContent tddContent) {
-        this.text = text;
-        this.file = file;
-        content = tddContent;
-    }
-
     public static Tdd blank() {
-        return new Tdd("[SAMPLE TDD TEXT LONG TEXT FORMAT]\nLine 2\nLine 3", null);
+        return new Tdd(SAMPLE_TDD_TEXT, null);
     }
 
     public Tdd withContent(final TddContent tddContent) {
@@ -62,11 +61,11 @@ public class Tdd {
         }
 
         public static TddId blank() {
-            return new TddId("[SAMPLE-TDD-ID]");
+            return new TddId(SAMPLE_BLANK_TDD_ID);
         }
 
         public static TddId noPr() {
-            return new TddId("no-PR");
+            return new TddId(SAMPLE_NO_PR_TDD_ID);
         }
 
         @Override
@@ -83,13 +82,14 @@ public class Tdd {
         private final String id;
 
         public static TddComponentReference blank() {
-            return new TddComponentReference("[SAMPLE-COMPONENT-ID]");
+            return new TddComponentReference(SAMPLE_BLANK_TDD_COMPONENT_REFERENCE);
         }
 
         @Override
         public String toString() {
+            // TODO: Implies that `toString` is used in business logic; it is
+            //       intended for debugging
             return id;
         }
     }
 }
-
