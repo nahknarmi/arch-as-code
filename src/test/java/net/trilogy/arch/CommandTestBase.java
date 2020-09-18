9df098f3caf4e4c312593cd8d25725f43dc0858d
Brian Oxley <boxley@thoughtworks.com>
2020-09-18 07:01:21 -0500

    Make it easier to debug STDOUT/STDERR

--- src/test/java/net/trilogy/arch/CommandTestBase.java
+++ src/test/java/net/trilogy/arch/CommandTestBase.java
@@ -1,20 +1,27 @@
 package net.trilogy.arch;
 
 import org.junit.Rule;
-import org.junit.Test;
 import org.junit.contrib.java.lang.system.SystemErrRule;
 import org.junit.contrib.java.lang.system.SystemOutRule;
 import org.junit.rules.ErrorCollector;
 
-import static net.trilogy.arch.TestHelper.execute;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.equalTo;
-
 public abstract class CommandTestBase {
+    /**
+     * When testing STDOUT and STDERR, toggle this.  The default should be
+     * {@code false}, so that passing tests do not dump STDOUT/STDERR to the
+     * console; setting it to {@code true} should still pass tests, but also
+     * print to the console at tests' conclusion.
+     */
+    public static final boolean DEBUG = false;
+
     @Rule
     public final ErrorCollector collector = new ErrorCollector();
     @Rule
-    public final SystemOutRule dummyOut = new SystemOutRule().enableLog().mute();
+    public final SystemOutRule dummyOut = DEBUG
+            ? new SystemOutRule().enableLog()
+            : new SystemOutRule().enableLog().mute();
     @Rule
-    public final SystemErrRule dummyErr = new SystemErrRule().enableLog().mute();
+    public final SystemErrRule dummyErr = DEBUG
+            ? new SystemErrRule().enableLog()
+            : new SystemErrRule().enableLog().mute();
 }
