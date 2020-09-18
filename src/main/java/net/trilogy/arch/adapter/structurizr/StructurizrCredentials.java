8602345b0911e743755f4f2e4e68e99bbf79abbd
Brian Oxley <boxley@thoughtworks.com>
2020-09-18 09:23:15 -0500

    More idiomatic Java
    Stream and Optional are awesome, however should not be used when there
    is a syntactic alternative unless sensible.

--- src/main/java/net/trilogy/arch/adapter/structurizr/StructurizrCredentials.java
+++ src/main/java/net/trilogy/arch/adapter/structurizr/StructurizrCredentials.java
@@ -1,23 +1,22 @@
 package net.trilogy.arch.adapter.structurizr;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableMap;
 import com.google.gson.Gson;
-import io.vavr.control.Try;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Map;
-import java.util.Optional;
 
-import static com.google.common.base.Preconditions.checkArgument;
+import static java.lang.String.format;
+import static java.lang.System.getenv;
 import static java.util.Collections.emptyMap;
-import static java.util.Optional.empty;
+import static java.util.Objects.requireNonNull;
 
-public abstract class StructurizrCredentials {
+public final class StructurizrCredentials {
     private static final String STRUCTURIZR_PATH = ".arch-as-code" + File.separator + "structurizr";
     private static final String CREDENTIALS_FILE_PATH = STRUCTURIZR_PATH + File.separator + "credentials.json";
 
@@ -26,70 +25,72 @@ public abstract class StructurizrCredentials {
     private static final String API_SECRET_ENV_VAR_NAME = "STRUCTURIZR_API_SECRET";
 
     public static WorkspaceConfig config() {
-        checkArgument(workspaceId().isPresent(), "Workspace id missing. Check config.");
-        checkArgument(apiKey().isPresent(), "Structurizr api key missing. Check config.");
-        checkArgument(apiSecret().isPresent(), "Structurizr api secret missing. Check config.");
-
         return WorkspaceConfig.builder()
-                .apiKey(apiKey().get())
-                .apiSecret(apiSecret().get())
-                .workspaceId(workspaceId().get())
+                .apiKey(requireNonNull(apiKey(), "Structurizr api key missing. Check config."))
+                .apiSecret(requireNonNull(apiSecret(), "Structurizr api key missing. Check config."))
+                .workspaceId(requireNonNull(workspaceId(), "Workspace id missing. Check config."))
                 .build();
     }
 
     // TODO [TESTING]: Add unit tests
-    public static void createCredentials(File productArchitectureDirectory, String workspaceId, String apiKey, String apiSecret) throws IOException {
-        String configPath = String.format("%s%s%s", productArchitectureDirectory.getAbsolutePath(), File.separator, STRUCTURIZR_PATH);
-        checkArgument(new File(configPath).mkdirs(), String.format("Unable to create directory %s.", configPath));
+    public static void createCredentials(
+            File productArchitectureDirectory,
+            String workspaceId,
+            String apiKey,
+            String apiSecret) throws IOException {
+        final var configPath = format("%s%s%s",
+                productArchitectureDirectory.getAbsolutePath(),
+                File.separator,
+                STRUCTURIZR_PATH);
+        if (!new File(configPath).mkdirs())
+            throw new IllegalArgumentException(format("Unable to create directory %s.", configPath));
 
-        File credentialsFile = new File(configPath + File.separator + "credentials.json");
+        final var credentialsFile = new File(configPath + File.separator + "credentials.json");
 
-        new ObjectMapper().writeValue(credentialsFile,
-                ImmutableMap.of(
-                        "workspace_id", workspaceId,
-                        "api_key", apiKey,
-                        "api_secret", apiSecret
-                )
-        );
+        new ObjectMapper().writeValue(credentialsFile, ImmutableMap.of(
+                "workspace_id", workspaceId,
+                "api_key", apiKey,
+                "api_secret", apiSecret));
     }
 
-    static Optional<FileInputStream> credentialsAsStream() {
-        return Try.of(() -> new FileInputStream(new File(CREDENTIALS_FILE_PATH)))
-                .map(Optional::of)
-                .getOrElse(empty());
+    static FileInputStream credentialsAsStream() {
+        try {
+            return new FileInputStream(CREDENTIALS_FILE_PATH);
+        } catch (final FileNotFoundException e) {
+            return null;
+        }
     }
 
-    static Optional<Long> workspaceId() {
-        return readWorkspaceDetail(WORKSPACE_ID_ENV_VAR_NAME, "workspace_id")
-                .map(Long::parseLong);
+    static Long workspaceId() {
+        final var workspaceId = readWorkspaceDetail(WORKSPACE_ID_ENV_VAR_NAME, "workspace_id");
+        return null == workspaceId ? null : Long.valueOf(workspaceId);
     }
 
-    static Optional<String> apiKey() {
+    static String apiKey() {
         return readWorkspaceDetail(API_KEY_ENV_VAR_NAME, "api_key");
     }
 
-    static Optional<String> apiSecret() {
+    static String apiSecret() {
         return readWorkspaceDetail(API_SECRET_ENV_VAR_NAME, "api_secret");
     }
 
-    static Optional<String> readWorkspaceDetail(String environmentVariableName, String jsonKey) {
-        String value = System.getenv().get(environmentVariableName);
+    static String readWorkspaceDetail(String environmentVariableName, String jsonKey) {
+        final var value = getenv().get(environmentVariableName);
         if (value != null) {
-            return Optional.of(value);
+            return value;
         } else if (!details().isEmpty()) {
-            return Optional.of(details().get(jsonKey));
+            return details().get(jsonKey);
         } else {
-            return empty();
+            return null;
         }
     }
 
     @SuppressWarnings("unchecked")
     static Map<String, String> details() {
-        return credentialsAsStream()
-                .map(InputStreamReader::new)
-                // TODO: Use Jackson not Gson
-                // TODO: Jackson sensibly throws IOException -- how to manage?
-                .map(x -> new Gson().fromJson(x, Map.class))
-                .orElse(emptyMap());
+        final var credentialsFile = credentialsAsStream();
+        if (null == credentialsFile) return emptyMap();
+        // TODO: Use Jackson not Gson
+        // TODO: Jackson sensibly throws IOException -- how to manage?
+        return (Map<String, String>) new Gson().fromJson(new InputStreamReader(credentialsFile), Map.class);
     }
 }
